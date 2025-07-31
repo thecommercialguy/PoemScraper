8d058bbf0de164174c742d3f877a7563a9562609
“William” <williamg28@gmail.com>
2025-07-31 16:14:49 -0500

    First batch

--- scraper.py
+++ scraper.py
@@ -1,40 +1,103 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import re
 
-# RETURNS A LIST OF POEM DIV'S
-# BASED OF DIV.POEM (GUTENBERG)
-def get_poems():
+# RETURNS A LIST OF POEM DIVS
+# BASED ON DIV.POEM (GUTENBERG)
+def get_poems(soup):
     poems = []
     for div in soup.find_all('div', 'poem'):  # get poems
+        schema_check = div.find('div', 'poem')
+        if schema_check:
+            continue
         poems.append(div)
         # if div.find('b'):
         #     print(div.find('b').get_text())
     return poems
 
 
-# RETURNS A LIST OF STANZAS (GUTENBERG)
-def get_stanzas():
-    poems = get_poems()
-    stanzas = []
-    # print(poems[0])
+# RETURNS A LIST OF STANZAS 
+# BASED ON DIV.POEM > DIV.STANZA (GUTENBERG)
+def get_stanzas(poem):
+    stanzas = poem.find_all('div', 'stanza')
+    return stanzas if len(stanzas) > 1 else None
 
-    for poem in poems:
-
-        stanzas.append(poem.find_all('div', 'stanza'))
-        print(len(poem.find_all('div', 'stanza')))
-
-    return stanzas
-
+# RETURNS A FORMATTED LIST OF POEM_OBJS
+# FROM DIV.POEM (GUTENBERG)
 def build_poems(poems):
-
+    poem_objs = []
     for poem in poems:
-        pass
+        poem_obj = build_poem(poem)
+        if poem_obj == None:
+            continue
+        poem_objs.append(poem_obj)
 
+    return poem_objs
+
+
+
+# RETURNS A POEM_OBJ 
+# FROM DIV.POEM (GUTENBERG)
 def build_poem(poem):
 
-    stanzas = get_stanzas(poem)
+    all_stanzas = get_stanzas(poem)  # all div.stanza elements
+    if not all_stanzas:
+        return
+    
+    # OBTAINING POEM.DIV
+    title_el = all_stanzas[0].css.select('span > b')
+    poet_el = all_stanzas[len(all_stanzas) - 1].css.select_one('span > i')
 
-    pass
+    # if len(title_el) > 1: 
+    #     return None
+
+    if not title_el:
+        return  None # may throw an error here 
+    
+    if not poet_el:
+        return None
+    
+    title = title_el[0].get_text()
+    poet = poet_el.get_text()
+    
+    body = all_stanzas[1:len(all_stanzas)-1]
+
+    stanzas = []
+
+    for stanza in body:
+        stanza_spans = stanza.find_all('span')
+        stanza_text = extract_text(stanza_spans)
+        if None in stanza_text:
+            return 
+        
+        stanzas.append(stanza_text)
+
+    if len(stanzas) < 1:
+        return None
+
+
+
+    poem_obj = {
+        "title": title,
+        "poet": poet,
+        "stanzas": stanzas
+    }
+
+    return poem_obj
+
+
+
+# RETURNS EXTRACTED TEXT FROM A LIST OF ELEMENTS
+def extract_text(els):
+    stanza_text = []
+    for el in els:
+        text = el.get_text()
+        if not text:
+            return None
+        stanza_text.append(text)
+
+    return stanza_text
 
 # RETURNS A TITLE
 def get_title():
@@ -60,11 +123,13 @@ soup = BeautifulSoup(html_data, 'html.parser')
 poems = get_poems(soup)
 
 
-build_poems(poems)
+poem_objs = build_poems(poems)
 
-soup.div
-# get_poems()
-get_title()
+with open("ouput.json", "w") as outfile:
+    json.dump(poem_objs, outfile, indent=4)
+print(len(poem_objs))
+
+
 
 
 
